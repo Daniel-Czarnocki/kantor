36228bac010d9fa375bdcad3a788eecb9f57b07f
Daniel Czarnocki <danielczarnocki@gmail.com>
2024-04-16 22:54:25 +0200

    refactoring and added functions

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
-let amountElement = document.querySelector(".js-amount");
-let rateElement = document.querySelector(".js-rate");
-let formElement = document.querySelector(".js-form");
-let resultElement = document.querySelector(".js-result");
-let currencyElement = document.querySelector(".js-currency");
+/*
+const amountElement = document.querySelector(".js-amount");
+const rateElement = document.querySelector(".js-rate");
+const formElement = document.querySelector(".js-form");
+const resultElement = document.querySelector(".js-result");
+const currencyElement = document.querySelector(".js-currency");
 console.log("Już w repozytorium GIT");
     
-    let usdRate = 4.39;
-    let eurRate = 4.66;
-    let gbpRate = 5.30;
-    let chfRate = 4.74;
+    const usdRate = 4.39;
+    const eurRate = 4.66;
+    const gbpRate = 5.30;
+    const chfRate = 4.74;
     
 
     formElement.addEventListener("submit", (event) => {
         event.preventDefault();
 
-        let amount = +amountElement.value;
+        const amount = +amountElement.value;
         let result;
 
     const checkedCurrencyElement = document.querySelector(".js-currency:checked");
@@ -42,4 +43,58 @@ console.log("Już w repozytorium GIT");
     
 });
 
+*/
+{
 
+
+    console.log("Już w repozytorium GIT");
+
+    const calculateResult = (amount, currency) => {
+        const usdRate = 4.07;
+        const eurRate = 4.32;
+        const gbpRate = 5.06;
+        const chfRate = 4.46;
+
+        switch (currency) {
+            case 'eur':
+                return amount / eurRate;
+            case 'usd':
+                return amount / usdRate;
+            case 'gbp':
+                return amount / gbpRate;
+            case 'chf':
+                return amount / chfRate;
+        }
+    };
+
+    const updateResultText = (result) => {
+        const resultElement = document.querySelector(".js-result");
+        resultElement.innerText = `${result.toFixed(2)}`;
+    };
+
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const amountElement = document.querySelector(".js-amount");
+        const checkedCurrencyElement = document.querySelector(".js-currency:checked");
+        
+
+        const amount = +amountElement.value;
+        const currency = checkedCurrencyElement?.value;
+
+        let result = calculateResult(amount, currency);
+
+        updateResultText(result);
+
+    };
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", onFormSubmit);
+
+    };
+
+    init();
+}
